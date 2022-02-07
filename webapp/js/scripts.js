a0017f35aee6fba0da5cdaa7b0204c3b0da80176
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-07 15:21:44 +0100

    Exercise chapter 1

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Initiales Script. Noch pssiert hier nichts");
+var button = document.getElementById("button");
+
+var rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+function change() {
+    document.body.style.background = rainbow[Math.floor(7*Math.random())];
+}
+button.addEventListener("click", change);
