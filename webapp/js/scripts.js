df1fe918e9fabc7cf3429f688be6ba74d3e7a66b
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-07 15:31:03 +0100

    Changes to syntax

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -3,6 +3,6 @@ var button = document.getElementById("button");
 var rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 function change() {
-    document.body.style.background = rainbow[Math.floor(7*Math.random())];
+    document.body.style.background = rainbow[Math.floor(7 * Math.random())];
 }
 button.addEventListener("click", change);
