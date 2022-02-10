d6f437d77eea6750e7f039268cc8bd818b23e632
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-10 16:55:51 +0100

    Commit of latest changes

--- quiz_ninja/js/scripts.js
+++ quiz_ninja/js/scripts.js
@@ -17,4 +17,24 @@ function play(quiz) {
     }
     // end of main game loop
     gameOver();
+
+    function ask(question) {
+        return prompt(question); // quiz[i][0]
+    }
+
+    function check(answer) {
+        if (answer === quiz[i][1]) { // quiz[i][1] is the i-th answer
+            alert("Correct!");
+            // increase score by 1
+            score++;
+        } else {
+            alert("Wrong?");
+        }
+    }
+
+    function gameOver() {
+        // inform the player that the game has finished an tell them how many points they have scored
+        alert("Game Over, you scored " + score + " points");
+    }
+
 }
