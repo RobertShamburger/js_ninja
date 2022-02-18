da21da8fcc5a45b95be3229736a604dbffb9c19b
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-18 12:28:55 +0100

    added eventremove

--- Chapters/Chapter_7/js/scripts.js
+++ Chapters/Chapter_7/js/scripts.js
@@ -45,6 +45,16 @@ addEventListener("touchend", function () {
     alert("Thank you!");
 });
 
+// remove event demo
+once = document.getElementById("once");
+once.addEventListener("click", remove);
+
+function remove(event){
+    console.log("Enjoy this while it lasts!");
+    once.style.backgroundColor = "pink";
+    once.removeEventListner("click", remove);
+}
+
 //==========================================================
 
 
