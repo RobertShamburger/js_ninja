a4ebde4872f85b0ed1823eefee4e6ff9d642d3e2
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 14:16:36 +0100

    powers commit

--- Chapters/Chapter_8/js/hero_script.js
+++ Chapters/Chapter_8/js/hero_script.js
@@ -1,11 +1,19 @@
 var form = document.forms.hero;
 form.addEventListener("submit", makeHero, false);
-
+form.powers[1].checked = true;
 function makeHero(event) {
     event.preventDefault(); // prevents submit action
 
     var hero = {};
     hero.name = form.name.value; // create a name property basedd on the input field's value
+    hero.realName = form.realName.value;
+
+    hero.powers = [];
+
+    for (i = 0; i < form.powers.length; i++) {
+        if (form.powers[i].checked)
+            hero.powers.push(form.powers[i].value);
+    }
 
     alert(JSON.stringify(hero)); // converts object to string and displays it in alert
 }
