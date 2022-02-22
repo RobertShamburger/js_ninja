c0fb8ef1e03d4df5415ded63ba49eb2a1be89978
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 11:46:01 +0100

    other events

--- Chapters/Chapter_8/js/scripts.js
+++ Chapters/Chapter_8/js/scripts.js
@@ -1,8 +1,8 @@
 var myForm = document.getElementsByTagName("form")[0];
-//var input = myForm["searchBox"];
-var input = myForm.elements[0];
-//var button = myForm["submitBtn"];
-var button = myForm.elements[1];
+var input = myForm["searchBox"];
+var button = myForm["submitBtn"];
 
-debugger;
+input.addEventListener("click", function(){console.log("Does the click event work work?")});
+console.log("Does anything work?");
+
 input.addEventListener("focus", function () { debugger; alert("focused") }, false);
