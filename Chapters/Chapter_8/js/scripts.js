60365644b6c08d7c3beebde1f8ec25d0984d5ec1
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 11:52:37 +0100

    please :(

--- Chapters/Chapter_8/js/scripts.js
+++ Chapters/Chapter_8/js/scripts.js
@@ -1,8 +1,8 @@
 var $myForm = document.getElementsByTagName("form")[0];
-var $input = myForm["searchBox"];
-var $button = myForm["submitBtn"];
+var $input = $myForm["searchBox"];
+var $button = $myForm["submitBtn"];
 
-input.addEventListener("click", function(){console.log("Does the click event work work?")});
+$input.addEventListener("click", function(){console.log("Does the click event work work?")});
 console.log("Does anything work?");
 
-input.addEventListener("focus", function () { debugger; alert("focused") }, false);
+$input.addEventListener("focus", function () { debugger; alert("focused") }, false);
