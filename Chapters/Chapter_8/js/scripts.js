31e07a7d812ba127cc8a434392db65a44ba16324
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 11:51:51 +0100

    poking in the dark

--- Chapters/Chapter_8/js/scripts.js
+++ Chapters/Chapter_8/js/scripts.js
@@ -1,6 +1,6 @@
-var myForm = document.getElementsByTagName("form")[0];
-var input = myForm["searchBox"];
-var button = myForm["submitBtn"];
+var $myForm = document.getElementsByTagName("form")[0];
+var $input = myForm["searchBox"];
+var $button = myForm["submitBtn"];
 
 input.addEventListener("click", function(){console.log("Does the click event work work?")});
 console.log("Does anything work?");
