bd4e285cecbb658a9004bacad799de4bf35d895e
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 13:13:49 +0100

    another commit

--- Chapters/Chapter_8/js/scripts.js
+++ Chapters/Chapter_8/js/scripts.js
@@ -2,8 +2,8 @@ var form = document.getElementsByTagName("form")[0];
 var input = form["searchBox"];
 var button = form["submitBtn"];
 
-input.addEventListener("focus", function () { alert("focused") }, false);
-input.addEventListener("blur", function () { alert("blurred") }, false);
+//input.addEventListener("focus", function () { alert("focused") }, false);
+//input.addEventListener("blur", function () { alert("blurred") }, false);
 input.addEventListener("change", function () { alert("changed") }, false);
 form.addEventListener("submit", function(event) {search(event)}, false);
 
