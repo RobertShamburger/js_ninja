8c61835eb77d2054f4f5834f652aa88a12a7f886
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 13:39:59 +0100

    Hero exercise

--- Chapters/Chapter_8/js/scripts.js
+++ Chapters/Chapter_8/js/scripts.js
@@ -1,15 +1,30 @@
 var form = document.getElementsByTagName("form")[0];
 var input = form["searchBox"];
+
+input.value = "Enter Search Term here";
 var button = form["submitBtn"];
 
 //input.addEventListener("focus", function () { alert("focused") }, false);
 //input.addEventListener("blur", function () { alert("blurred") }, false);
 input.addEventListener("change", function () { alert("changed") }, false);
-form.addEventListener("submit", function(event) {search(event)}, false);
+form.addEventListener("submit", function (event) { search(event) }, false);
+// Empty input field on focus
+input.addEventListener("focus", function() {
+    if (input.value === "Enter Search Term here") {
+        input.value = "";
+    }
+}, false);
+// Fill input field with default text on focus loss
+input.addEventListener("blur", function() {
+    if (input.value = "") {
+        input.value = "Enter Search Term here";
+    }
+}, false);
 
 function search(event) {
     //debugger;
-    alert("You Searched for: " + input.value);
-    event.preventDefault();
-    
+    if (input.value !== "Enter Search Term here") {
+        alert("You Searched for: " + input.value);
+        event.preventDefault();
+    }
 }
