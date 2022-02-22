21c370a7f433c250d31d88395358792dfae05431
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 10:59:52 +0100

    debugger

--- Chapters/Chapter_8/js/scripts.js
+++ Chapters/Chapter_8/js/scripts.js
@@ -2,4 +2,5 @@ var form = document.getElementsByTagname('form')["search"];
 var input = form.elements["searchBox"];
 var button = form.elements["submitBtn"];
 
-input.addEventListener("focus", function(){alert("focused")}, false);
+
+input.addEventListener("focus", function(){debugger; alert("focused")}, false);
