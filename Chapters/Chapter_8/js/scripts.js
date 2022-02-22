1f615494156f9471b7a930ee896a4cc1284f042d
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 11:29:58 +0100

    I don't get it!!!

--- Chapters/Chapter_8/js/scripts.js
+++ Chapters/Chapter_8/js/scripts.js
@@ -1,6 +1,6 @@
-var myForm = document.forms["search"];
-var input = myForm["searchBox"];
-var button = myForm["submitBtn"];
+myForm = document.getElementsByTagname('form')["search"];
+input = myForm["searchBox"];
+button = myForm["submitBtn"];
 
 debugger;
 input.addEventListener("focus", function () { debugger; alert("focused") }, false);
