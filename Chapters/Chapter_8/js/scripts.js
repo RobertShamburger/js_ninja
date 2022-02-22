92b09ff81b09b55adbe059b44b12e315cb4d51ec
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 11:36:25 +0100

    I suck

--- Chapters/Chapter_8/js/scripts.js
+++ Chapters/Chapter_8/js/scripts.js
@@ -1,6 +1,8 @@
-myForm = document.getElementsByTagname('form')["search"];
-input = myForm["searchBox"];
-button = myForm["submitBtn"];
+var myForm = document.getElementsByTagName("form")[0];
+//var input = myForm["searchBox"];
+var input = myForm.elements[0];
+//var button = myForm["submitBtn"];
+var button = myForm.elements[1];
 
 debugger;
 input.addEventListener("focus", function () { debugger; alert("focused") }, false);
