35cc0cd5db48c30880590c275f19a16a26cc62e1
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 11:55:58 +0100

    give me anything!

--- Chapters/Chapter_8/js/scripts.js
+++ Chapters/Chapter_8/js/scripts.js
@@ -1,8 +1,11 @@
 var $myForm = document.getElementsByTagName("form")[0];
 var $input = $myForm["searchBox"];
 var $button = $myForm["submitBtn"];
+var para = document.getElementById("mainParagraph");
 
 $input.addEventListener("click", function(){console.log("Does the click event work work?")});
 console.log("Does anything work?");
 
 $input.addEventListener("focus", function () { debugger; alert("focused") }, false);
+
+para.addEventListener("click", function (){console.log("The paragraph was clicked")});
