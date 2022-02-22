c2aff380f338dbc3594eecf71b0180fb7b6a4a87
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 12:57:14 +0100

    bypassed submit

--- Chapters/Chapter_8/js/scripts.js
+++ Chapters/Chapter_8/js/scripts.js
@@ -5,3 +5,11 @@ var button = form["submitBtn"];
 input.addEventListener("focus", function () { alert("focused") }, false);
 input.addEventListener("blur", function () { alert("blurred") }, false);
 input.addEventListener("change", function () { alert("changed") }, false);
+form.addEventListener("submit", search(event), false);
+
+function search(event) {
+    debugger;
+    event.preventDefault();
+    alert("Form Submitted");
+    return false;
+}
