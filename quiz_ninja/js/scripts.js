82f2a7a2339ecf206837850a8e87f0fab8ca4685
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 16:43:06 +0100

    Quiz Ninja Ch8

--- quiz_ninja/js/scripts.js
+++ quiz_ninja/js/scripts.js
@@ -3,19 +3,12 @@
 var $question = document.getElementById("question");
 var $score = document.getElementById("score");
 var $feedback = document.getElementById("feedback");
-// view functions
-function update(element, content, klass) {
-    var p = element.firstChild || document.createElement("p");
-    p.textContent = content;
-    element.appendChild(p);
-    if(klass) {
-        p.className = klass;
-    }
-}
-
+var $form = document.getElementById("answer");
+var $start = document.getElementById("start");
+// form is hidden before start of game
+hide($form);
 
 // program refs //
-var $start = document.getElementById("start");
 var quiz = {
     "name": "Super Hero Name Quiz",
     "description": "How many super heroes can you name?",
@@ -29,25 +22,65 @@ var quiz = {
 
 var score = 0;
 // Event Listner
-$start.addEventListener("click", function(){ play(quiz), false});
+$start.addEventListener("click", function () { play(quiz), false });
 
-//play(quiz);
+// utility functions
+function hide(element) {
+    element.style.display = "none";
+}
+
+function show(element) {
+    element.style.display = "block";
+}
+
+// main game functions 
+
+function update(element, content, klass) {
+    var p = element.firstChild || document.createElement("p");
+    p.textContent = content;
+    element.appendChild(p);
+    if (klass) {
+        p.className = klass;
+    }
+}
+
 
 function play(quiz) {
+    hide($start); // hides start button
+    show($form);
     update($score, score);
-    // main game loop
-    for (var i = 0, question, answer, max = quiz.questions.length; i < max; i++) {
-        question = quiz.questions[i].question;
-        answer = ask(question);
-        check(answer);
-    }
 
-    // end of main game loop
-    gameOver();
+    // main game loop
+    var i = 0;
+    // Asks the first question. Starts the game loop
+    chooseQuestion();
+
+    /* Disabled for the time being. Delete if obsolete.  
+      for (var i = 0, question, answer, max = quiz.questions.length; i < max; i++) {
+          question = quiz.questions[i].question;
+          answer = ask(question);
+          check(answer);
+      }
+  
+      gameOver();
+      // end of main game loop */
+
+    // EventListner that checks for the users input
+    $form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        check($form[0].value);
+    }, false);
+
+    function chooseQuestion() {
+        var question = quiz.questions[i].question;
+        ask(question)
+    }
 
     function ask(question) {
         update($question, quiz.question + question);
-        return prompt(quiz.question + question);
+        $form[0].value = "";
+        $form[0].focus();
+        // return prompt(quiz.question + question);
     }
 
     function check(answer) {
@@ -59,9 +92,18 @@ function play(quiz) {
         } else {
             update($feedback, "Wrong!", "wrong");
         }
+
+        i++;
+        if (i === quiz.question.length) {
+            gameOver();
+        } else {
+            chooseQuestion();
+        }
     }
 
     function gameOver() {
+        show($start);
+        hide($form);
         // inform the player that the game has finished an tell them how many points they have scored
         update($question, "Game Over, you scored " + score + " points");
     }
