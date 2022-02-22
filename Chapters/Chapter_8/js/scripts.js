6e2bbfcc9e7799f79f756d5e297e5aabff836bc0
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-02-22 11:10:13 +0100

    change

--- Chapters/Chapter_8/js/scripts.js
+++ Chapters/Chapter_8/js/scripts.js
@@ -1,7 +1,6 @@
-//var form = document.getElementsByTagname('form')["search"];
-var form = document.getElementById("search");
+var form = document.getElementsByTagname('form')["search"];
 var input = form.elements["searchBox"];
 var button = form.elements["submitBtn"];
 
 
-input.addEventListener("focus", function(){debugger; alert("focused")}, false);
+input.addEventListener("focus", function () { debugger; alert("focused") }, false);
